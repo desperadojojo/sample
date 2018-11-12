d35a279c6c9ebe6d86ccd5ca302f12f1ecd7ae88
desperadojojo <25340029@qq.com>
2018-11-12 10:39:23 +0000

    Add user following

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Notifications\ResetPassword;
+use Auth;
 
 class User extends Authenticatable
 {
@@ -30,7 +31,7 @@ class User extends Authenticatable
 
     protected $table = 'users';
 
-     public function gravatar($size = '100')
+    public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
@@ -55,10 +56,49 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class,'followers','user_id','follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class,'followers','follower_id','user_id');
+    }
+
+    //TODO: belongsToMany ？
+
+    /**
+     * @param $user_ids
+     */
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids,false);
+    }
+
+    public function unfollow($user_ids){
+         if (!is_array($user_ids)){
+             $user_ids = compact('user_ids');
+         }
+         $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id){
+        return $this->followings->contains($user_id);
+    }
+
     public function feed()
     {
-        return $this->statuses()
-            ->orderBy('created_at', 'desc');
+        $user_ids = Auth::user()->followings->pluck('id')->toArray();
+        array_push($user_ids,Auth::user()->id);
+        return Status::whereIn('user_id',$user_ids)
+            ->with('user')
+            ->orderBy('created_at','desc');
+
     }
+
 
 }
